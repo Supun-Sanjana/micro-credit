c85fc6fa19bd7768c3e54539cc13bfa060cd860e
Supun <infor.ssupun@gmail.com>
2026-09-25 20:34:21 +0530

    chore: remove script files

--- debug.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const file = 'app/api/loans/[id]/events/route.ts'
-let content = fs.readFileSync(file, 'utf8')
-const startStr = '// --- ACCOUNTING ---'
-let idx1 = content.indexOf(startStr)
-console.log(content.substring(idx1, idx1 + 1000))
