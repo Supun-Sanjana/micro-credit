667391f1c76511ec5ef0a5180c74a23474540599
Supun <infor.ssupun@gmail.com>
2026-09-25 20:59:41 +0530

    chore: strictly remove temp js scripts

--- test_server.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
-async function run() {
-  const account = await prisma.savingsAccount.findFirst();
-  console.log(account)
-}
-run()
